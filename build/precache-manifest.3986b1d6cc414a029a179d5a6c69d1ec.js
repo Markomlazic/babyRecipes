19f3ea52513c0d64e1e6fcb8122a783789176527
Markomlazic <44021239+Markomlazic@users.noreply.github.com>
2020-04-24 13:06:50 +0200

    add build folder

---
+++ build/precache-manifest.3986b1d6cc414a029a179d5a6c69d1ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab65a7c25c300dbe86ef41fbb98e11ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5c090229a00a0636382",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "64138f0c30c9b2180917",
+    "url": "/static/css/main.cebe6dda.chunk.css"
+  },
+  {
+    "revision": "f5c090229a00a0636382",
+    "url": "/static/js/2.d0f7a8f3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.d0f7a8f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64138f0c30c9b2180917",
+    "url": "/static/js/main.10c19c7d.chunk.js"
+  },
+  {
+    "revision": "3806438a95c0f5514c00",
+    "url": "/static/js/runtime-main.c9c8962a.js"
+  }
+]);
